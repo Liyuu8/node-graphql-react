397aa32fa546b4a10a8165503f851c6ceb59c508
Liyuu <13977245+Liyuu8@users.noreply.github.com>
2020-11-03 19:41:24 +0900

    Update side nav ui and function

--- client/src/components/SideNav.js
+++ client/src/components/SideNav.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { useQuery, useMutation } from '@apollo/react-hooks';
 import { useForm } from 'react-hook-form';
-import { DIRECTOR_LIST, ADD_MOVIE } from '../queries/queries';
+import {
+  DIRECTOR_LIST,
+  ADD_MOVIE,
+  MOVIE_LIST,
+  ADD_DIRECTOR,
+} from '../queries/queries';
 import {
   Button,
   Card,
@@ -14,24 +19,47 @@ import {
 const SideNav = () => {
   // https://sbfl.net/blog/2019/08/26/javascript-bracketed-variables/
   const { data } = useQuery(DIRECTOR_LIST);
-  const { register, handleSubmit } = useForm();
-  const [addMovie] = useMutation(ADD_MOVIE);
-  const onSubmit = ({ movieName, movieGenre, directorId }) =>
+  const {
+    register: registerMovie,
+    handleSubmit: handleSubmitMovie,
+  } = useForm();
+  const {
+    register: registerDirector,
+    handleSubmit: handleSubmitDirector,
+  } = useForm();
+  const [addMovie] = useMutation(ADD_MOVIE, {
+    refetchQueries: [{ query: MOVIE_LIST }],
+    awaitRefetchQueries: true,
+  });
+  const [addDirector] = useMutation(ADD_DIRECTOR, {
+    refetchQueries: [{ query: DIRECTOR_LIST }],
+    awaitRefetchQueries: true,
+  });
+  const onSubmitMovie = ({ movieName, movieGenre, directorId }, e) => {
     addMovie({ variables: { name: movieName, genre: movieGenre, directorId } });
+    e.target.reset();
+  };
+  const onSubmitDirector = ({ directorName, directorAge }, e) => {
+    addDirector({
+      variables: { name: directorName, age: parseInt(directorAge) },
+    });
+    e.target.reset();
+  };
 
   return (
     <div>
-      {/* TODO: 入力フォーム（映画監督） */}
+      {/* 入力フォーム（映画監督） */}
       <Card>
         <CardHeader>映画監督</CardHeader>
         <CardBody>
-          <Form>
+          <Form onSubmit={handleSubmitDirector(onSubmitDirector)}>
             <FormGroup>
               <input
                 type="text"
                 name="directorName"
                 className="form-control"
                 placeholder="監督名"
+                ref={registerDirector}
               />
             </FormGroup>
             <FormGroup>
@@ -40,6 +68,7 @@ const SideNav = () => {
                 name="directorAge"
                 className="form-control"
                 placeholder="年齢"
+                ref={registerDirector}
               />
             </FormGroup>
             <Button color="primary" type="submit">
@@ -50,18 +79,17 @@ const SideNav = () => {
       </Card>
 
       {/* 入力フォーム（映画作品） */}
-      {/* TODO: 登録時の再読み込み処理 */}
       <Card className="mt-4">
         <CardHeader>映画作品</CardHeader>
         <CardBody>
-          <Form onSubmit={handleSubmit(onSubmit)}>
+          <Form onSubmit={handleSubmitMovie(onSubmitMovie)}>
             <FormGroup>
               <input
                 type="text"
                 name="movieName"
                 className="form-control"
                 placeholder="タイトル"
-                ref={register}
+                ref={registerMovie}
               />
             </FormGroup>
             <FormGroup>
@@ -70,7 +98,7 @@ const SideNav = () => {
                 name="movieGenre"
                 className="form-control"
                 placeholder="ジャンル"
-                ref={register}
+                ref={registerMovie}
               />
             </FormGroup>
             <FormGroup>
@@ -78,7 +106,7 @@ const SideNav = () => {
                 className="form-control"
                 type="select"
                 name="directorId"
-                ref={register}
+                ref={registerMovie}
               >
                 {data &&
                   data.directors.map(({ id, name }) => (
